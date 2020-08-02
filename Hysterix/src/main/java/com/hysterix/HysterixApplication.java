b4ccb97799169103600398332dc6570365a5214f
deepakrajan <deepakrajan@deepaks-mbp>
2020-08-02 00:31:33 -0700

    updated intial code

---
+++ Hysterix/src/main/java/com/hysterix/HysterixApplication.java
@@ -0,0 +1,13 @@
+package com.hysterix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HysterixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HysterixApplication.class, args);
+	}
+
+}
